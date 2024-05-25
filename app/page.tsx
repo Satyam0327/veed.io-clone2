72377957c4fec08e104c1f09e008fac70fead7ae
SatyamGuptaDev <devsatyamg@gmail.com>
2024-05-25 23:46:53 +0530

    chore: Add Next.js Image Component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    
+    <div>
+      <h1>Next.js Image Component</h1>
+      <Image
+        src="/images/nextjs.png"
+        alt="Next.js Logo"
+        width={500}
+        height={500}
+      />
+    </div>
   );
 }
