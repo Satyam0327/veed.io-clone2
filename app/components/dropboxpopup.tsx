01316c5dfddf5ded164973996abf00937ab5ea12
Satyam0327 <satyamsingh0327@gmail.com>
2024-05-26 15:30:27 +0530

    done

--- app/components/dropboxpopup.tsx
+++ app/components/dropboxpopup.tsx
@@ -2,17 +2,18 @@ import { useRef } from 'react';
 import { Text, Group, Button, rem, useMantineTheme } from '@mantine/core';
 import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
 import { IconCloudUpload, IconX, IconDownload } from '@tabler/icons-react';
+import classes from './DropzoneButton.module.css';
 
 export function DropzoneButton() {
   const theme = useMantineTheme();
   const openRef = useRef<() => void>(null);
 
   return (
-    <div className="relative mb-8 bg-black text-white p-4 rounded-md">
+    <div className={classes.wrapper}>
       <Dropzone
         openRef={openRef}
         onDrop={() => {}}
-        className="border border-gray-300 pb-12 rounded-md bg-white text-black"
+        className={classes.dropzone}
         radius="md"
         accept={[MIME_TYPES.pdf]}
         maxSize={30 * 1024 ** 2}
@@ -34,30 +35,22 @@ export function DropzoneButton() {
               />
             </Dropzone.Reject>
             <Dropzone.Idle>
-              <IconCloudUpload style={{ width: rem(50), height: rem(50) }} stroke={1.5} />
+              <IconCloudUpload style={{ width: rem(50), height: rem(50),marginLeft:"160px" }} stroke={1.5} />
             </Dropzone.Idle>
           </Group>
 
           <Text ta="center" fw={700} fz="lg" mt="xl">
             <Dropzone.Accept>Drop files here</Dropzone.Accept>
             <Dropzone.Reject>Pdf file less than 30mb</Dropzone.Reject>
-            <Dropzone.Idle>Upload resume</Dropzone.Idle>
+            <Dropzone.Idle>Upload Video</Dropzone.Idle>
           </Text>
           <Text ta="center" fz="sm" mt="xs" c="dimmed">
-            Drag&apos;n&apos;drop files here to upload. We can accept only <i>.pdf</i> files that
-            are less than 30mb in size.
+            choose files or Drag&apos;n&apos;drop files here to upload.
           </Text>
         </div>
       </Dropzone>
 
-      <Button
-        className="absolute w-64 left-1/2 transform -translate-x-1/2 -bottom-5 bg-blue-500 hover:bg-blue-600 text-white"
-        size="md"
-        radius="xl"
-        onClick={() => openRef.current?.()}
-      >
-        Select files
-      </Button>
+      
     </div>
   );
 }
